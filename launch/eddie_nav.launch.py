551d283739363decee777fb5be78b358b46afefc
Vamsi <vamsikalagaturu@gmail.com>
2024-11-28 20:03:43 +0100

    Update navigation configuration and launch files for eddie robot

--- launch/eddie_nav.launch.py
+++ launch/eddie_nav.launch.py
@@ -1,20 +1,21 @@
 from simple_launch import SimpleLauncher
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_share_directory
+import os
+
 
 def generate_launch_description():
-
     sl = SimpleLauncher(use_sim_time=True)
 
     # launch eddie simulation
     sl.include('eddie_gazebo', 'run_sim.launch.py')
 
     # rviz
-    sl.rviz(sl.find('eddie_navigation', 'eddie_gz.rviz', 'config/rviz'))
+    sl.rviz(sl.find("eddie_navigation", "eddie_gz.rviz", "config/rviz"))
 
-    # launch slam
-    sl.include('slam_toolbox', 'online_async_launch.py', launch_arguments={
-        'use_sim_time': 'true',
-        'autostart': 'true',
-        # 'params_file': sl.find('eddie_navigation', 'nav2_params.yaml', 'config')
-    })
+    # localization
+    sl.node('robot_localization', 'ekf_node', parameters=[sl.find("eddie_navigation", "ekf.yaml", "config")])
+
 
     return sl.launch_description()
