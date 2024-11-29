d030a1d910dc3a541b54f57b8cd44a65878706e0
Vamsi <vamsikalagaturu@gmail.com>
2024-11-29 11:42:54 +0100

    Refactor navigation and localization launch files; rename slam_toolbox.yaml to mapper_params_online_async.yaml and update parameters for localization mode

---
+++ launch/localization.launch.py
@@ -0,0 +1,27 @@
+from simple_launch import SimpleLauncher
+
+
+def generate_launch_description():
+    sl = SimpleLauncher(use_sim_time=True)
+
+    # launch eddie simulation
+    sl.include("eddie_gazebo", "run_sim.launch.py")
+
+    # rviz
+    sl.rviz(sl.find("eddie_navigation", "eddie_gz.rviz", "config/rviz"))
+
+    # localization
+    sl.node(
+        "robot_localization",
+        "ekf_node",
+        parameters=[sl.find("eddie_navigation", "ekf.yaml", "config")],
+    )
+
+    sl.include("eddie_navigation", "slam.launch.py", launch_arguments={
+        "use_sim_time": "true",
+        "autostart": "true",
+        "use_lifecycle_manager": "false",
+        "slam_params_file": sl.find("eddie_navigation", "mapper_params_online_async.yaml", "config")
+    })
+
+    return sl.launch_description()
--- launch/nav.launch.py
+++ launch/nav.launch.py
@@ -1,21 +1,8 @@
 from simple_launch import SimpleLauncher
-from launch.actions import IncludeLaunchDescription
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from ament_index_python.packages import get_package_share_directory
-import os
 
 def generate_launch_description():
     sl = SimpleLauncher(use_sim_time=True)
 
-    # sl.include(
-    #     "eddie_navigation",
-    #     "navigation.launch.py",
-    #     launch_arguments={
-    #         "use_sim_time": "true",
-    #         "autostart": "true",
-    #         "use_respawn": "true",
-    #         "params_file": sl.find("eddie_navigation", "nav2_params.yaml", "config"),
-    #     },
-    # )
+
 
     return sl.launch_description()
